6b4c441cd1c3cddedd1c3f5d0033a0087c420a1c
holguin4857 <omar@trexco.net>
2023-03-22 16:32:17 +0100

    css

--- components/Footer.js
+++ components/Footer.js
@@ -6,30 +6,9 @@ export default function Footer () {
         <footer className="footer">
           <div className="cont">
             <div className="row">
-              <div className="footer-col">
-                <h4>Terms</h4>
-                <ul>
-                  <li><Link href="terms">Terms and conditions</Link></li>
-                  <li><Link href="pri">Privacy policy</Link></li>
-                  
-                </ul>
-              </div>
-              <div className="footer-col">
-                <h4>About</h4>
-                <ul>
-                  <li><Link href="/">Home</Link></li>
-                  <li><Link href="about">About</Link></li>
-                  <li><Link href="contact">Contact</Link></li>
-                </ul>
-              </div>
-              <div className="footer-col">
-                <h4>Booking</h4>
-                <ul>
-                  <li><Link href="work">Work</Link></li>
-                  <li><Link href="how">FAQ</Link></li>
-                  
-                </ul>
-              </div>
+             
+             
+             
               <div className="footer-col">
                 <h4>Follow</h4>
                 <ul>
